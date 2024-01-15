1c74f7d7e9cf1cd633152e9423bddfe87d6f2872
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2024-01-15 14:22:46 +0200

    change server url

--- src/pages/forum/ChatPage/ChatPage.tsx
+++ src/pages/forum/ChatPage/ChatPage.tsx
@@ -199,7 +199,7 @@ const ChatPage = () => {
                 </Flex>
 
 
-                <StompSessionProvider url={'http://localhost:6060/ws-endpoint'}>
+                <StompSessionProvider url={'https://v2.miloverada.gov.ua:8443/ws-endpoint'}>
                     <ChatWindow typingUsers={typingUsers}
                                 setTypingUsers={setTypingUsers}
                                 chat={chat}
