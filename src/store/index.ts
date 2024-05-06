be38b1d9e4bf049750c2e75ae7b2dd74c32f8d57
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2024-05-06 23:32:14 +0300

    remove dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,7 @@
-import {applyMiddleware, legacy_createStore as createStore, compose} from 'redux'
+import {applyMiddleware, legacy_createStore as createStore} from 'redux'
 import {thunk} from "redux-thunk";
 import {rootReducer} from "./reducers";
-import {composeWithDevTools} from "@redux-devtools/extension";
 
-//
-const composedEnhancer = compose(applyMiddleware(thunk), composeWithDevTools())
 
 // @ts-ignore
-export const store = createStore(rootReducer, composedEnhancer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
