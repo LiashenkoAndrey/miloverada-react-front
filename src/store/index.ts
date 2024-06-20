80942f660b57ecd13e3b08d770fba86191b317e0
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2024-06-20 13:52:46 +0300

    create store without dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,6 @@ import {thunk} from "redux-thunk";
 import {rootReducer} from "./reducers";
 import {composeWithDevTools} from "@redux-devtools/extension";
 
-const composedEnhancers = composeWithDevTools(applyMiddleware(thunk))
+// const composedEnhancers = composeWithDevTools(applyMiddleware(thunk))
 // @ts-ignore
-export const store = createStore(rootReducer, composedEnhancers)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
